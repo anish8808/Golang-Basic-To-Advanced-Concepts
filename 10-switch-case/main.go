269f2f4ac33f296c8a6989a4670ffa343d302723
anish8808 <anishkashyap11111@gmail.com>
2024-12-05 23:43:47 +0530

    Added the switch code

--- 10-switch-case/main.go
+++ 10-switch-case/main.go
@@ -1,6 +1,9 @@
 package main
 
+import "math/rand"
+
 func main() {
+	//if you want to achive something that other programiing wihtout break it will run till default case
 	var day uint8 = 5
 	switch day {
 	case 1:
@@ -22,7 +25,7 @@ func main() {
 	}
 
 	//or
-	switch day := 4; day {
+	switch day := 3; day {
 	case 1:
 		println("Sunday") //break is default in each case
 	case 2:
@@ -40,44 +43,36 @@ func main() {
 	default:
 		println("Noday")
 	}
+	for i := 0; i < 10; i++ {
+		num := rand.Intn(2000)
 
-	switch day := 4; day {
-	case 1:
-		println("Sunday") //break is default in each case
-	case 2:
-		println("Monday")
-	case 3:
-		println("TuesDay")
-	case 4:
-		println("thursday")
-	case 5:
-		println("wednesday")
-	case 6:
-		println("friday")
-	case 7:
-		println("Saturday")
-	default:
-		println("Noday")
+		switch {
+		case num >= 50 && num < 100:
+			println("The number is grater then 50 and less then 100")
+		case num >= 100 && num < 200:
+			println("The number is grater then 100 and less then 200")
+		default:
+			println("num is greatrer then 200 or less then 50")
+		}
+	}
+	str := "Hello Worlds"
+	count := 0
+	for _, char := range str {
+		switch char {
+		case 'A', 'E', 'I', 'O', 'U', 'a', 'e', 'i', 'o', 'u':
+			count++
+			println(string(char), "is an Wovel")
+		default:
+			println(string(char), "is a constent")
+		}
 	}
 
-	switch day := 4; day {
-	case 1:
-		println("Sunday") //break is default in each case
-	case 2:
-		println("Monday")
-	case 3:
-		println("TuesDay")
-	case 4:
-		println("thursday")
-	case 5:
-		println("wednesday")
-	case 6:
-		println("friday")
-	case 7:
-		println("Saturday")
-	default:
-		println("Noday")
-	}
+	println("Number of wovels in a given String is ", count)
 
-	println("this is the switch case statement in golang ")
+	var ch int32 = 65
+	if ch == 'A' {
+		println("YES")
+	} else {
+		println("NO")
+	}
 }
